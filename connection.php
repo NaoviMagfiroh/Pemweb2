774e7060449bf5bb788e6f4782d9ccee83bae09a
NaoviMagfiroh <80589413+NaoviMagfiroh@users.noreply.github.com>
2022-03-30 14:01:37 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -2,9 +2,9 @@
 
 $hostDB = '127.0.0.1';
 $portDB = '5432';
-$nameDB = 'wp2_4d';
-$userDB = 'postgres';
-$pwDB = 'naovi29';
+$nameDB = 'jihwegxb';
+$userDB = 'jihwegxb';
+$pwDB = 'WwiUIea8mSoDnk3iIbdjsVz9E73OmLbd';
 
 $connection = pg_connect("host=$hostDB port=$portDB dbname=$nameDB user=$userDB password=$pwDB");
 
